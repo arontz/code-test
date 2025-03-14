2914f0f131584938557ffc1a4cf5720f0b87f4f8
Baharun <en.baharun@gmail.com>
2025-03-15 00:10:50 +0800

    triangle function

--- 9-TheTriangle/triangle.php
+++ 9-TheTriangle/triangle.php
@@ -4,11 +4,16 @@ $num = trim(fgets(STDIN));
 
 
 if (is_numeric($num)) {
-    echo "You entered a number: $num\n";
+    printCenteredTriangle($num);
 } else {
     echo "This is not a number!\n";
     exit;
 }
 
-
-echo $num;
+function printCenteredTriangle($num) {
+    for ($i = 1; $i <= $num; $i++) {
+        $spaces = str_repeat(' ', $num - $i); // Spaces before stars
+        $stars = str_repeat('*', (2 * $i) - 1); // Odd number of stars
+        echo $spaces . $stars . "\n";
+    }
+}
